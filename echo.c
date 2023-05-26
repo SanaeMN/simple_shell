e4af436b7623e9d91fdcc3068917c468a9223bd7
SanaeMN <sanaeenemuom2017@gmail.com>
2023-05-26 15:43:33 -0700

    handle echo

--- echo.c
+++ echo.c
@@ -7,7 +7,7 @@
  */
 int echo(char **vars)
 {
-	int sts;
+	int status;
 
 	if (!_strcmp(vars[0], "echo") && vars[1])
 	{
@@ -19,8 +19,8 @@ int echo(char **vars)
 		}
 		else if (!_strcmp(vars[1], "$?"))
 		{
-			waitpid(getpid() - 1, &sts, 0);
-			to_string(1, WEXITSTATUS(sts));
+			waitpid(getpid() - 1, &status, 0);
+			to_string(1, WEXITSTATUS(status));
 			write(STDOUT_FILENO, "\n", 1);
 			return (1);
 		}
